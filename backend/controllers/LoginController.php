794284716c60e991c9e4129f576b62b2d0dbd839
Lucas Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2026-09-19 22:51:11 -0300

    backend: crear LoginController y registrar rutas de autenticación

---
+++ backend/controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Controllers;
+
+class LoginController
+{
+    public static function login()
+    {
+        echo 'desde login prueba';
+    }
+    public static function logout()
+    {
+        echo 'desde logout prueba';
+    }
+    public static function olvide()
+    {
+        echo 'desde olvide prueba';
+    }
+    public static function recuperar()
+    {
+        echo 'desde recuperar prueba';
+    }
+    public static function crear()
+    {
+        echo 'desde crear cuenta prueba';
+    }
+}
