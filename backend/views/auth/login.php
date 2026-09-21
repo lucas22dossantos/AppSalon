0b38f714d963d131032a4326f4b5e4d0637d6c69
Lucas Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2026-09-20 23:09:51 -0300

    backend: crear formulario de login

--- backend/views/auth/login.php
+++ backend/views/auth/login.php
@@ -1,2 +1,23 @@
+<?php
+
+use Controllers\LoginController; ?>
+
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesion con tus datos</p>
+
+<form class="fomulario" method="POST" action="">
+    <div class="campo">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Tu email" required>
+    </div>
+    <div class="campo">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="contraseña" required>
+    </div>
+    <input type="submit" class="boton" value="Iniciar Sesion">
+</form>
+
+<div class="acciones">
+    <a href="crear-cuenta">Crear Cuenta</a>
+    <a href="olvide">¿olvidaste tu contraseña?</a>
+</div>
